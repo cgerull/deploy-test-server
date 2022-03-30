eb039463a6b90ae0b210a8110d7abc5fd8424e81
Claus Gerull <claus.gerull@solvinity.com>
2022-03-30 08:30:06 +0200

    Add basic pipeline.

---
+++ .cicd/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash --debug
+#
+
+# For assume that every chart is deployed to every envrionment.
+# Later we use Helmfile to make deployment more clearly.
+#
+# Get charts
+for chart in $(ls charts); do 
+  # First do a static analysis
+  helm lint charts/${chart}
+  # Create kubernetes manifests per environment
+  for env in $(ls deploy); do 
+    echo "Extract manifest ${chart} for ${env}."
+    helm template charts/${chart} --values=deploy/${env}/${chart}.yaml > kubernetes/${env}/${chart}.yaml
+  done
+done
+
+# Validate manifests
+# if [ $(kubectl get nodes 2&1 > /dev/nulls) ]; then
+#     for env in $(ls deploy); do
+#         kubectl apply -f kubernetes/${env}/${chart}.yaml --dry-run=client
+#     done
+# fi
+    
