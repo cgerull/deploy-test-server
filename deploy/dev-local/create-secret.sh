596fdedfef34acc432c8fd0ccc3679653dc8f1be
Claus <cgerull@live.com>
2023-11-10 10:16:47 +0100

    Clean argocd dev, add secret scripts dev-local. (#21)

--- deploy/dev-local/create-secret.sh
+++ deploy/dev-local/create-secret.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 #
 # Create a simple testsecret
-kubectl create secret generic testserver --from-literal=testserver=mytestsecret
+# kubectl create secret generic testserver --from-literal=testserver=mytestsecret
+#
+# Create ACME DNS secret
+kubectl create secret generic acme-dns --from-file $HOME/deploy/dev-local/acmedns.json
