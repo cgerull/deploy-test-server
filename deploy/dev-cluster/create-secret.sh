ae5b07d27b8563556b4fd378a658519b4266563e
Claus Gerull <cgerull@live.com>
2023-12-21 20:30:44 +0100

    Fixed sealed secret.

--- deploy/dev-cluster/create-secret.sh
+++ deploy/dev-cluster/create-secret.sh
@@ -3,4 +3,5 @@
 # Create a simple testsecret
 kubectl create secret generic testserver --from-literal=testserver=dev-cluster-secret --namespace=testserver --dry-run=client -o yaml > /tmp/testsecret
 kubectl config set-context --current --namespace=testserver
-kubeseal --format yaml < /tmp/testsecret > sealed-testsecret.yaml
+kubeseal --scope namespace-wide --format yaml < /tmp/testsecret > sealed-testsecret.yaml
+rm /tmp/testsecret
